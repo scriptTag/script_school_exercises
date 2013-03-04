d1605ba7f876ba33c8c21925abc7517c4a3a626f
Mike Stipanov <mike@dreamwoods.com>
2013-03-03 17:16:47 -0700

    Current JS

---
+++ javascripts/application.js
@@ -0,0 +1,14 @@
+function myName (nameOne, nameTwo) {
+	var combinedNames = joinedStrings(nameOne, nameTwo);
+	console.log(combinedNames);
+}
+
+
+function joinedStrings(string1, string2) {
+	return string1 + " & " + string2;
+}
+
+function myTwoFavoriteSongs(song1, song2) {
+	var joinedSongs = joinedStrings(song1, song2);
+	console.log(joinedSongs);
+}
